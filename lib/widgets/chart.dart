bd40a8266bc55fc490d283d5c9f6503f94ed9906
Paing-Thaw <paingthaw2096@gmail.com>
2023-02-27 15:56:19 +0630

    3:57 27/2/2023

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -3,10 +3,12 @@ import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
   // const Chart({Key? key}) : super(key: key);
+  Chart(this.recentTransactions);
+
   final List<Transaction> recentTransactions;
   List<Map<String, Object>> get groupedTransactionValues {
-    List.generate(7, (index) => {
-      return {'day': 'T', 'amount': 99.9}
+    return List.generate(7, (index)  {
+      return {'day': 'T', 'amount': 99.9};
     });
   }
 
